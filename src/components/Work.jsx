efb807c863e06b437a3f2674e8afb0a620f9f028
abdulmajidabdalah <abdulmajidabdalah27@gmail.com>
2023-01-26 12:05:17 +0700

    second Commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import IMG2 from "./assets/home/portfolio.jpg";
-import Editorial from "./assets/thumb_product/editorial.jpeg";
-import Printing from "./assets/thumb_product/printing.jpeg";
-import Merchandise from "./assets/thumb_product/merchandise.jpeg";
-import PhotoVideo from "./assets/thumb_product/photovideo.jpeg";
+import Editorial from "./assets/thumb_product/editorial.png";
+import Printing from "./assets/thumb_product/printing.png";
+import Merchandise from "./assets/thumb_product/merchandise.png";
+import PhotoVideo from "./assets/thumb_product/photovideo.png";
 import { FiArrowDown } from "react-icons/fi";
 import { Link as Button } from "react-scroll";
 
@@ -15,39 +15,43 @@ const Work = () => {
         <div className="lg:grid lg:grid-cols-3 md:grid md:grid-cols-3 bg-[#F2F2F2] h-[100vh] md:h-full lg:h-auto">
           <main className="col-span-2 mx-4 lg:mx-[68px] lg:mt-[62px]">
             <p className="text-6xl md:text-8xl lg:text-8xl text-white font-poppins font-bold tracking-wider pt-[50px] lg:pt-0">
-              Portofolio
+              Portfolio
             </p>
             <p className="font-extrabold font-poppins text-2xl md:text-4xl lg:text-4xl text-[#333C4E] tracking-wider mt-2 lg:mt-0">
               Our Project
             </p>
             <div className="grid grid-cols-2 lg:grid lg:grid-cols-2 gap-[20px] mt-[70px] lg:mt-[95px]">
               <Link to="/editorial" className="group relative">
-                <img src={Editorial} alt="" />
-                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:opacity-80 group-hover:cursor-pointer duration-500">
+                <img src={Editorial} alt="" className="ml-[-1rem] mt-[1rem]" />
+                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:rounded-md group-hover:opacity-80 group-hover:cursor-pointer duration-500">
                   <h1 className="uppercase text-[#F4AD24] text-xl font-semibold tracking-widest ">
                     editorial
                   </h1>
                 </div>
               </Link>
               <Link to="/printing" className="group relative">
-                <img src={Printing} alt="" />
-                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:opacity-80 group-hover:cursor-pointer duration-500">
+                <img src={Printing} alt="" className="ml-[-1rem] mt-[1rem]" />
+                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:rounded-md group-hover:opacity-80 group-hover:cursor-pointer duration-500">
                   <h1 className="uppercase text-[#F4AD24] text-xl font-semibold tracking-widest ">
                     printing
                   </h1>
                 </div>
               </Link>
               <Link to="/merchandise" className="group relative">
-                <img src={Merchandise} alt="" />
-                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:opacity-80 group-hover:cursor-pointer duration-500">
+                <img
+                  src={Merchandise}
+                  alt=""
+                  className="ml-[-1rem] mt-[1rem]"
+                />
+                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:rounded-md group-hover:opacity-80 group-hover:cursor-pointer duration-500">
                   <h1 className="uppercase text-[#F4AD24] text-xl font-semibold tracking-widest ">
                     merchandise
                   </h1>
                 </div>
               </Link>
               <Link to="/photo&video" className="group relative">
-                <img src={PhotoVideo} alt="" />
-                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:opacity-80 group-hover:cursor-pointer duration-500">
+                <img src={PhotoVideo} alt="" className="ml-[-1rem] mt-[1rem]" />
+                <div className="hidden absolute top-0 left-0 w-full h-0 lg:flex flex-col justify-center items-center bg-[#121111] opacity-0 group-hover:h-full group-hover:rounded-md group-hover:opacity-80 group-hover:cursor-pointer duration-500">
                   <h1 className="uppercase text-[#F4AD24] text-xl font-semibold tracking-widest ">
                     photo & video
                   </h1>
--- src/page/ProductsDetails/Merchandise/Souvenir.jsx
+++ src/page/ProductsDetails/Merchandise/Souvenir.jsx
@@ -4,12 +4,13 @@ import Footer from "../../../components/Footer";
 import BackToTop from "../../../components/BackToTop";
 import AsideDetailProduct from "../../../components/AsideDetailProduct";
 import { SlideshowLightbox } from "lightbox.js-react";
-import IMG1 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/PEN Set.jpg";
-import IMG2 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/POWERBANK.jpg";
-import IMG3 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/SOUVENIR Batik Pigura.jpg";
-import IMG4 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/SOUVENIR Jam.jpg";
-import IMG5 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/SOUVENIR plakat kayu.jpg";
-import IMG6 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/SOUVENIR Plakat.jpg";
+import IMG1 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/souvenir.jpeg";
+import IMG2 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/souvenir2.jpeg";
+import IMG3 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/souvenir3.jpeg";
+import IMG4 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/souvenir4.jpeg";
+import IMG5 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/souvenir5.jpeg";
+import IMG6 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/souvenir6.jpeg";
+import IMG7 from "../../../components/assets/products/03 MERCHANDISE/01 SOUVENIR/souvenir1.jpeg";
 import { IoMdCloseCircle } from "react-icons/io";
 import NavbarProduct from "../../../components/NavbarProduct";
 
@@ -32,8 +33,8 @@ const Souvenir = () => {
         </Helmet>
         <section className="flex">
           <AsideDetailProduct />
-          <div className="w-[100%] mt-2">
-            <main className="px-4 lg:h-screen">
+          <div className="w-full mt-2">
+            <main className="px-4">
               <h1 className="my-4 text-xl font-bold hidden lg:block">
                 Souvenir
               </h1>
@@ -52,7 +53,8 @@ const Souvenir = () => {
                 <img className="lg:mt-0 mt-4" src={IMG3} alt="" />
                 <img className="lg:mt-0 mt-4" src={IMG4} alt="" />
                 <img className="lg:mt-0 mt-4" src={IMG5} alt="" />
-                <img className="lg:mt-0 mt-4 mb-4" src={IMG6} alt="" />
+                <img className="lg:mt-0 mt-4" src={IMG6} alt="" />
+                <img className="lg:mt-0 mt-4 mb-4" src={IMG7} alt="" />
               </SlideshowLightbox>
             </main>
             <BackToTop />
